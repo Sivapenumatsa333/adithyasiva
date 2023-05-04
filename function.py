ff1d67aa214d67d7e3f5286741ab6900c45f17a7
Sivapenumatsa333 <88231439+Sivapenumatsa333@users.noreply.github.com>
2023-05-05 00:57:11 +0530

    Update function.py

--- function.py
+++ function.py
@@ -1,32 +1,50 @@
-# function.py:
-import json
-import psycopg2
-import psycopg2.extras
-import os
+// This scripts take all files from the source folder and
+// copy them to S3 bucket
+
+import { S3, PutObjectCommand } from "@aws-sdk/client-s3";
+import { Upload } from '@aws-sdk/lib-storage';
+import fs from "fs";
+import path from "path";
+//const fs = require('fs');
+// const path = require('path');
 
 
-def lambda_handler(event, context):
-    db_name = os.environ['DB_NAME']
-    db_user = os.environ['DB_USER']
-    db_host = os.environ['DB_HOST']
-    db_port = os.environ['DB_PORT']
-    db_pass = os.environ['DB_PASS']
+// set credentials and region
+// const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
+// const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+// const AWS_DEFAULT_REGION = process.env.AWS_DEFAULT_REGION;
+// AWS.config.update({
+//   accessKeyId: AWS_ACCESS_KEY_ID,
+//   secretAccessKey: AWS_SECRET_ACCESS_KEY,
+//   region: AWS_DEFAULT_REGION,
+// });
+
+const s3 = new S3();
+
+// const BUCKET_NAME = 'adithyasiva.shop';
+const BUCKET_NAME = 'prueba.test.sh';
+const SOURCE_FOLDER = 'source';
 
 
-    conn = psycopg2.connect(user=db_user, database=db_name, host=db_host,
-                            password=db_pass, port=db_port)
+export const handler = async(event) => {
+    const files = fs.readdirSync(SOURCE_FOLDER);
+    
+    for (const file of files) {
+      const filePath = path.join(SOURCE_FOLDER, file);
+      const fileContent = fs.readFileSync(filePath);
 
-
-    sql = "SELECT * FROM stocks_intraday ORDER BY time DESC LIMIT 10"
-    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    cursor.execute(sql)
-    result = cursor.fetchall()
-
-
-    return {
-        'statusCode': 200,
-        'body': json.dumps(list_of_dicts, default=str),
-        'headers': {
-            "Content-Type": "application/json"
-        }
+      const params = {
+        Bucket: BUCKET_NAME,
+        Key: file,
+        Body: fileContent,
+      };
+      const command = new PutObjectCommand(params);
+      await s3.send(command);
     }
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Copied!!'),
+    };
+    return response;
+};
